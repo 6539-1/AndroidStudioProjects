052313ce0440ba753ab4c176c8307570ef48bd0e
xritian <865439513@qq.com>
2018-07-19 11:14:14 +0800

    Add some database method,get inf from db and show them on the screen(whj)

--- JSIP/app/src/main/java/com/example/administrator/jsip/SignIn.java
+++ JSIP/app/src/main/java/com/example/administrator/jsip/SignIn.java
@@ -8,6 +8,8 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
@@ -15,6 +17,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,10 +36,17 @@ public class SignIn extends AppCompatActivity{
     private EditText PasswordView;
     private CheckBox is_show_psw;
     private TextView signUpBtn;
+    private EditText Accounttext;
+    private ImageView HeadView;
     private SharedPreferences prefs;
     private ArrayList<Personal> personals;
     private String Id;
     private SignIn.InnerReceiver receiver = new SignIn.InnerReceiver();
+    private int[] arrImages = new int[]{
+            R.mipmap.img1,R.mipmap.img2,R.mipmap.img3,
+            R.mipmap.img4,R.mipmap.img5, R.mipmap.img6,
+            R.mipmap.img7,R.mipmap.img8,R.mipmap.img9
+    };
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         /*
@@ -47,20 +57,41 @@ public class SignIn extends AppCompatActivity{
         customHeaders.put("customHeader1","customValue1");
         customHeaders.put("customHeader2","customValue2");
         DeviceImpl.getInstance().Initialize(getApplicationContext(), sipProfile,customHeaders);
-
-
+        SQLManeger.getSqlManeger().init(this);
         startService(new Intent(this,MyService.class));
+
         onRestart();
         setContentView(R.layout.sign_in);
         AccountView = findViewById(R.id.dropview);
-        setAccountList();
-        SQLManeger.getSqlManeger().init(this);
+        Accounttext = findViewById(R.id.Account);
+        HeadView = findViewById((R.id.logo));
         personals=SQLManeger.getSqlManeger().Personalquery();
-        //SQLManeger.getSqlManeger().closeDatabase();
+        setAccountList();
 
         if(AccountList.size()!=0) {
             AccountView.setDefaultAccount(AccountList);
         }
+        Accounttext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                for (int i=0;i<AccountList.size();i++){
+                    if (charSequence.toString().equals(Accounttext.getText())){
+                        HeadView.setImageResource(arrImages[personals.get(i).getImage_id()]);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         PasswordView = findViewById(R.id.psd_view);
         signInBtn = findViewById(R.id.sign_in_btn);
         signInBtn.setOnClickListener(new View.OnClickListener() {
@@ -100,12 +131,12 @@ public class SignIn extends AppCompatActivity{
 
     public void setAccountList() {
         AccountList=new ArrayList<>();
-        if(personals!=null) {
+        if(personals.size()!=0) {
             for (int i = 0; i < personals.size(); i++) {
                 AccountList.add(Integer.toString(personals.get(i).getId()));
             }
         }
-        AccountList.add("123456");
+        //AccountList.add("123456");
     }
 
     @Override
@@ -130,7 +161,12 @@ public class SignIn extends AppCompatActivity{
             String nickname=intent.getStringExtra("nickname");
             int pic_owner=intent.getIntExtra("pic_owner",-1);
             if(is_SignIn==0) {
-
+                TextView Accout = findViewById(R.id.Account);
+                String thisId = Accout.getText().toString();
+                Personal personal=new Personal(nickname,Integer.parseInt(thisId),pic_owner);
+                if (!containPerson(personals,personal))
+                    personals.add(0,personal);
+                SQLManeger.getSqlManeger().addPerson(personals);
                 DeviceImpl.getInstance().SendMessage(ServiceIp,"$flush");
                 DeviceImpl.getInstance().SendMessage(ServiceIp,"$message");
                 Intent intent_Id = new Intent(SignIn.this, MainActivity.class);
@@ -147,6 +183,21 @@ public class SignIn extends AppCompatActivity{
                 Toast.makeText(SignIn.this,"密码有误", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    public boolean containPerson(ArrayList<Personal> persons,Personal person){
+        boolean result = false,a=false,b=false,c=false;
+
+        for (int i = 0; i<persons.size();i++){
+            a = persons.get(i).getImage_id()==person.getImage_id();
+            b = persons.get(i).getId()==person.getId();
+            c = persons.get(i).getNickname().equals(person.getNickname());
+            if(a&&b&&c) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
 }
